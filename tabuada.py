e28967fda304e61ebf9679c48812d97cff8bde92
leandrosilva19 <leandrosilva19@gmail.com>
2023-08-31 08:36:31 -0300

    Criado programa para fazer Tabuada

---
+++ tabuada.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""Imprime a tabuada do 1 ao 10."""
+__version__ = "0.1.0"
+__author__ = "Leandro"
+
+#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1,11))
+
+#Iterable (percorriveis)
+
+#para cada em numeros:
+for numero in numeros:
+	print("Tabuada do:",numero)
+	for outro_numero in numeros:
+		print(numero * outro_numero)
+	
